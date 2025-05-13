a6c7597bbd35be093600e8e94d705b555cb102ea
yannamsiva <yannamsiva@gmail.com>
2025-05-14 00:10:58 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcome To Ashok IT...!!
+        </p>
+        <a
+          className="App-link"
+          href="https://ashokitech.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+         Visit Our Website
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
