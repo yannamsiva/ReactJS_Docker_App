acc252d3661c51dd6fbc8bcaec6b77778f612e0d
yannamsiva <46184940+yannamsiva@users.noreply.github.com>
2025-05-14 00:26:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome To Ashok IT...!!
+          Welcome To Ajacs Coding Tuator...!!
         </p>
         <a
           className="App-link"
-          href="https://ashokitech.com/"
+          href="www.ajacs.in"
           target="_blank"
           rel="noopener noreferrer"
         >
